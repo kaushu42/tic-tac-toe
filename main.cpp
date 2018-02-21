f4720ddeba443021d148a2681dc0396a2ad67d63
Kaushal Raj Mishra <kaushu42@gmail.com>
2018-02-21 18:03:33 +0545

    Added ability to draw X and O  on click

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <vector>
 #include <SFML/Graphics.hpp>
+float sqrt2 = 1.414213562;
 float windowWidth = 800, windowHeight = 600;
+std::vector<sf::Shape*> s;
+void drawO(sf::RenderWindow& window, int xPos, int yPos){
+  float radius = 25;
+  sf::CircleShape *c = new sf::CircleShape(radius);
+  c->setOrigin(radius, radius);
+  c->setOutlineThickness(10);
+  c->setFillColor(sf::Color(0, 0, 0, 0));
+  c->move(xPos, yPos);
+  s.push_back(c);
+}
+
+void drawItems(){}
+void drawX(sf::RenderWindow& window, int xPos, int yPos){
+  float width = 10, height = 50 * sqrt2;
+  sf::RectangleShape* r = new sf::RectangleShape(sf::Vector2f(width, height));
+  r->setOrigin(width/2, height/2);
+  r->move(xPos, yPos);
+  r->rotate(45);
+  s.push_back(r);
+  sf::RectangleShape* r1 = new sf::RectangleShape(*r);
+  r->rotate(-90);
+  s.push_back(r1);
+}
+
 void drawBoard(sf::RenderWindow& window){
   float width = 10, height = 300;
   float gap = 100;
@@ -25,17 +51,27 @@ int main(int argc, char const *argv[]) {
   window.setVerticalSyncEnabled(true);
   sf::CircleShape r(100.0f);
   r.setOrigin(r.getPosition().x + r.getRadius(), r.getPosition().y  + r.getRadius());
-  window.clear(sf::Color(120, 140, 160));
-  drawBoard(window);
-  while(window.isOpen()){
 
+  while(window.isOpen()){
     sf::Event e;
+    window.clear(sf::Color(120, 140, 160));
+    drawBoard(window);
+    for(auto& i:s){
+      window.draw(*i);
+    }
+    drawItems();
     while(window.pollEvent(e)){
       if(e.type == sf::Event::Closed){
         window.close();
       }else if(e.type == sf::Event::MouseButtonPressed){
+          float xPos = e.mouseButton.x, yPos = e.mouseButton.y;
           if(e.mouseButton.button == sf::Mouse::Left){
-            std::cout << e.mouseButton.x << ',' << e.mouseButton.y << std::endl;
+            std::cout << xPos << ',' << yPos << std::endl;
+            drawX(window, xPos, yPos);
+          }
+          if(e.mouseButton.button == sf::Mouse::Right){
+            std::cout << xPos << ',' << yPos << std::endl;
+            drawO(window, xPos, yPos);
           }
         }
     }
