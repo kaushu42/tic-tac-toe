0b956b7aa710b295b818fa36b6c1cb1238d55c49
kaush <kaushu42@gmail.com>
2018-08-16 19:16:16 +0545

    Added Comments and limited the number of shapes

--- main.cpp
+++ main.cpp
@@ -1,94 +1,129 @@
 #include <iostream>
 #include <vector>
 #include <SFML/Graphics.hpp>
-float sqrt2 = 1.414213562;
-float windowWidth = 800, windowHeight = 600;
-std::vector<sf::Shape*> s;
-void drawO(sf::RenderWindow& window, int xPos, int yPos){
-  float radius = 25;
+
+const float sqrt2 = 1.414213562;
+const float windowWidth = 800, windowHeight = 600;
+
+std::vector<sf::Shape *> s;
+
+// Draw O on the screen
+void drawO(sf::RenderWindow &window, int xPos, int yPos)
+{
+  float radius = 25; // The radius of the circle to be drawn
+  // Create the circle
   sf::CircleShape *c = new sf::CircleShape(radius);
   c->setOrigin(radius, radius);
   c->setOutlineThickness(10);
   c->setFillColor(sf::Color(0, 0, 0, 0));
   c->move(xPos, yPos);
+  // Push the circle to the vector containing all the shapes
   s.push_back(c);
 }
 
-void drawItems(){}
-void drawX(sf::RenderWindow& window, int xPos, int yPos){
-  float width = 10, height = 50 * sqrt2;
-  sf::RectangleShape* r = new sf::RectangleShape(sf::Vector2f(width, height));
-  r->setOrigin(width/2, height/2);
+void drawItems() {}
+
+// Draw X's on the screen
+void drawX(sf::RenderWindow &window, int xPos, int yPos)
+{
+  float width = 10, height = 50 * sqrt2; // The parameters of the Cross
+  // Create the first rectangle for the cross shape
+  sf::RectangleShape *r = new sf::RectangleShape(sf::Vector2f(width, height));
+  r->setOrigin(width / 2, height / 2);
   r->move(xPos, yPos);
   r->rotate(45);
+  // Push the rectangle to the vector containing all the shapes
   s.push_back(r);
-  sf::RectangleShape* r1 = new sf::RectangleShape(*r);
+  // Create the second rectangle for the cross shape
+  sf::RectangleShape *r1 = new sf::RectangleShape(*r);
   r->rotate(-90);
+  // Push the rectangle to the vector containing all the shapes
   s.push_back(r1);
 }
 
-void drawBoard(sf::RenderWindow& window){
+// Draw the board from vertical and horizontal lines
+void drawBoard(sf::RenderWindow &window)
+{
+  // The width and height of vertical lines
   float width = 10, height = 300;
+  // The gap between each line that makes up the board
   float gap = 100;
+
+  // Create the rectangles
   sf::RectangleShape l(sf::Vector2f(width, height));
 
-  //Draw the Vertical Lines
+  // Draw the Vertical Lines
   l.setFillColor(sf::Color(200, 50, 50));
-  l.move(windowWidth/2 - gap/2 , windowHeight/2 - height/2);
+  l.move(windowWidth / 2 - gap / 2, windowHeight / 2 - height / 2);
   window.draw(l);
   l.move(gap, 0);
   window.draw(l);
-  //Draw the Horizontal Lines
+
+  // Draw the Horizontal Lines
   l.setSize(sf::Vector2f(height, width));
   l.setFillColor(sf::Color(50, 50, 200));
-  l.setPosition(windowWidth/2 - height/2 , windowHeight/2 - gap/2);
+  l.setPosition(windowWidth / 2 - height / 2, windowHeight / 2 - gap / 2);
   window.draw(l);
   l.move(0, gap);
   window.draw(l);
 }
-int main(int argc, char const *argv[]) {
-  sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight, 32), "Testing SFML", sf::Style::Titlebar| sf::Style::Close);
-  window.setVerticalSyncEnabled(true);
-  sf::CircleShape r(100.0f);
-  r.setOrigin(r.getPosition().x + r.getRadius(), r.getPosition().y  + r.getRadius());
 
-  while(window.isOpen()){
+int main(int argc, char const *argv[])
+{
+  // Create the main window
+  sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight, 32), "Testing SFML", sf::Style::Titlebar | sf::Style::Close);
+  window.setVerticalSyncEnabled(true);
+  // sf::CircleShape r(100.0f);
+  // r.setOrigin(r.getPosition().x + r.getRadius(), r.getPosition().y + r.getRadius());
+
+  // Count how many shapes have been placed
+  int shapesCount = 0;
+
+  // Main game loop
+  while (window.isOpen())
+  {
+    // Create a event to handle all keyboard and mouse actions
     sf::Event e;
+    // Set the background color
     window.clear(sf::Color(120, 140, 160));
+    // Draw the board
     drawBoard(window);
-    for(auto& i:s){
+    // For all shapes, in 's', draw them one after another
+    for (auto &i : s)
+    {
       window.draw(*i);
     }
-    drawItems();
-    while(window.pollEvent(e)){
-      if(e.type == sf::Event::Closed){
+    // Check for events
+    while (window.pollEvent(e))
+    {
+      // If the user closes the window, exit the game
+      if (e.type == sf::Event::Closed)
+      {
         window.close();
-      }else if(e.type == sf::Event::MouseButtonPressed){
+      }
+      if (shapesCount < 9)
+      {
+        if (e.type == sf::Event::MouseButtonPressed) // Handle mouse presses
+        {
+          // Get the current mouse position
           float xPos = e.mouseButton.x, yPos = e.mouseButton.y;
-          if(e.mouseButton.button == sf::Mouse::Left){
+
+          if (e.mouseButton.button == sf::Mouse::Left)
+          {
             std::cout << xPos << ',' << yPos << std::endl;
             drawX(window, xPos, yPos);
           }
-          if(e.mouseButton.button == sf::Mouse::Right){
+          if (e.mouseButton.button == sf::Mouse::Right)
+          {
             std::cout << xPos << ',' << yPos << std::endl;
             drawO(window, xPos, yPos);
           }
+          shapesCount++;
         }
+      }
     }
     window.display();
   }
 
   return 0;
 }
-
-/*if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-{
-    // left key is pressed: move our character
-    character.move(1, 0);
-}
-if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
-{
-    // left mouse button is pressed: shoot
-    gun.fire();
-}
-*/
